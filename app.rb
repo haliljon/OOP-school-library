271538266641f8bbbc99a4b76eb04a9445ee6693
haliljon <110017001+haliljon@users.noreply.github.com>
2023-03-21 17:50:14 +0500

    Refactor the code using SOLID principles

--- app.rb
+++ app.rb
@@ -1,117 +1,36 @@
-require_relative 'student'
-require_relative 'teacher'
-require_relative 'book'
-require_relative 'rental'
-require_relative 'classroom'
+require_relative 'app/list_rentals'
+require_relative 'app/list_books'
+require_relative 'app/list_people'
 
 class App
-  attr_accessor :books, :people, :rentals, :classroom
-
   def initialize
-    @books = []
-    @people = []
-    @rentals = []
-    @classroom = ['Web Development']
-  end
-
-  def get_user_input_int(message)
-    puts message
-    gets.chomp.to_i
-  end
-
-  def get_user_input_string(message)
-    puts message
-    gets.chomp.to_s
+    @list_people = ListPeople.new
+    @list_books = ListBooks.new
+    @list_rentals = ListRentals.new
   end
 
   def list_all_books
-    puts 'Please add a book to the library' if @books.empty?
-    @books.each do |book|
-      puts "Title: #{book.title}, Author: #{book.author}"
-    end
+    @list_books.list_books
   end
 
   def list_all_people
-    puts 'Please add a person to the library' if @people.empty?
-    @people.each do |person|
-      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
+    @list_people.list_all_people
   end
 
   def create_person
-    input = get_user_input_int('Do you want to create a student (1) or a teacher (2)? [Input the number]:')
-
-    case input
-    when 1
-      create_student
-    when 2
-      create_teacher
-    else
-      invalid_input
-      create_person
-    end
-  end
-
-  def create_student
-    age = get_user_input_int('Age:')
-    name = get_user_input_string('Name:')
-    has_permission = get_user_input_string('Has parent permission? [Y/N]:').upcase
-    case has_permission
-    when 'Y'
-      permission = true
-    when 'N'
-      permission = false
-    else
-      invalid_input
-      create_student
-    end
-
-    @people << Student.new(classroom, age, name, parent_permission: permission)
-    puts 'Person created successfully'
-  end
-
-  def create_teacher
-    age = get_user_input_int('Age:')
-    name = get_user_input_string('Name:')
-    specialization = get_user_input_string('Specialization:')
-
-    @people << Teacher.new(specialization, age, name)
-    puts 'Person created successfully'
+    @list_people.create_person
   end
 
   def create_book
-    title = get_user_input_string('Title:')
-    author = get_user_input_string('Author:')
-    books << Book.new(title, author)
-    puts 'Book created successfully'
+    @list_books.create_book
   end
 
   def create_rental
-    puts 'Select a book from the following list by number'
-    books.each_with_index do |book, index|
-      puts "#{index}) Title: #{book.title}, Author: #{book.author}"
-    end
-    book_index = gets.chomp.to_i
-    puts 'Select a person from the following list by number (not id)'
-    people.each_with_index do |person, index|
-      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-    person_index = gets.chomp.to_i
-    date = get_user_input_string('Date:')
-    rentals << Rental.new(date, people[person_index], books[book_index])
-    puts 'Rental created successfully'
+    @list_rentals.create_rental(@list_books.books, @list_people.people)
   end
 
   def list_all_rentals_for_person_id
-    id = get_user_input_int('ID of person:')
-    puts 'Rentals:'
-    rentals.each do |rental|
-      if rental.person.id == id
-        puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
-      else
-        puts 'There are no rentals for that ID'
-      end
-    end
+    @list_rentals.list_all_rentals_for_person_id
   end
 
   def invalid_input
---
+++ app/list_people.rb
@@ -0,0 +1,65 @@
+require_relative '../student'
+require_relative '../teacher'
+
+class ListPeople
+  attr_accessor :people, :classroom
+
+  def initialize
+    @classroom = ['Web Development']
+    @people = []
+  end
+
+  def list_all_people
+    @people.each do |person|
+      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
+
+  def create_person
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+    input = gets.chomp.to_i
+
+    case input
+    when 1
+      create_student
+    when 2
+      create_teacher
+    else
+      invalid_input
+      create_person
+    end
+  end
+
+  def create_student
+    puts 'Age:'
+    age = gets.chomp.to_i
+    puts 'Name:'
+    name = gets.chomp.to_s
+    puts 'Has parent permission? [Y/N]:'
+    has_permission = gets.chomp.to_s.upcase
+    case has_permission
+    when 'Y'
+      permission = true
+    when 'N'
+      permission = false
+    else
+      invalid_input
+      create_student
+    end
+
+    @people << Student.new(classroom, age, name, parent_permission: permission)
+    puts 'Person created successfully'
+  end
+
+  def create_teacher
+    puts 'Age:'
+    age = gets.chomp.to_i
+    puts 'Name:'
+    name = gets.chomp.to_s
+    puts 'Specialization:'
+    specialization = gets.chomp.to_s
+
+    @people << Teacher.new(specialization, name, age)
+    puts 'Person created successfully'
+  end
+end
